f806e72812ee7b51b890aa1bc87fff4f11173c2c
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-07 11:40:07 +0530

    Added the scripts for the toggling the play btn icon..

--- script.js
+++ script.js
@@ -13,7 +13,11 @@ function toggleVideoStatus() {
 }
 
 function togglePlayIcon() {
-  return true;
+  if (video.paused) {
+    play.innerHTML = `<i class="fa fa-play fa-2x"></i>`;
+  } else {
+    play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`;
+  }
 }
 
 function updateProgress() {
